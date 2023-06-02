57da5eb40979c2f2ea55e686bdc16b8b4512b888
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2023-06-02 14:35:32 -0300

    Atualizando as docstrings do acordes

--- notas_musicais/acordes.py
+++ notas_musicais/acordes.py
@@ -1,3 +1,8 @@
+"""
+Módulo de acordes.
+
+O módulo de acordes conta com funções e feramentas necessárias para a geração de acordes.
+"""
 from notas_musicais.escalas import NOTAS, escala
 
 
@@ -13,12 +18,46 @@ def _menor(cifra):
     return notas, graus
 
 
-def semiton(nota, intervalo):
+def semiton(nota, intervalo) -> list[str]:
+    """
+    Calcula a distancia em semitons entre duas notas usando intervalos.
+
+    Parameters:
+        nota: Nota de referência
+        intervalo: Intervalo em semitons
+
+    Returns:
+        Uma nota correspondente ao intervalo dado
+
+    Examples:
+        >>> semiton('C', intervalo=+1)
+        'C#'
+
+        >>> semiton('C', intervalo=-1)
+        'B'
+    """
     pos = NOTAS.index(nota)
     return NOTAS[(pos + intervalo) % len(NOTAS)]
 
 
-def triade(nota, tonalidade):
+def triade(nota, tonalidade) -> list[str]:
+    """
+    Gera tríades de uma nota tônica e uma tonalidade.
+
+    Parameters:
+        nota: Uma nota da qual se deseja obter a tríade
+        tonalidade: Uma tonalidade na qual será formado o acorde
+
+    Returns:
+        A tríade do acorde referente a nota e tonalidade
+
+    Examples:
+        >>> triade('C', 'maior')
+        ['C', 'E', 'G']
+
+        >>> triade('C', 'menor')
+        ['C', 'D#', 'G']
+    """
     graus = (0, 2, 4)
     notas_da_escala, _ = escala(nota, tonalidade).values()
 
@@ -28,10 +67,13 @@ def triade(nota, tonalidade):
 def acorde(cifra: str) -> dict[str, list[str]]:
     """
     Gera as notas de um acorde a partir de uma cifra
+
     Parameters:
         cifra: Cifra de um acorde
+
     Returns:
         Um dicionário com as notas e graus do acorde
+
     Examples:
         >>> acorde('C')
         {'notas': ['C', 'E', 'G'], 'graus': ['I', 'III', 'V']}
